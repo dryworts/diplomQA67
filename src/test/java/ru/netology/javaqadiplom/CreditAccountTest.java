da82244961474844c7a95b7d41506435a19e80c7
dryworts <omzg@mail.ru>
2023-09-08 18:01:29 +0600

    some tests added into CreditAccountTest

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -17,4 +17,38 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
+
+    @Test
+    public void shouldAddButInitialMoreThanZero() {
+        CreditAccount account = new CreditAccount(
+                100,
+                3_000,
+                15
+        );
+
+        account.add(3_000);
+
+        Assertions.assertEquals(3100, account.getBalance());
+//Метод add неверно реализован, т.к. должно быть не balance = amount, а balance += amount
+    }
+
+
+    @Test
+    public void shouldAddButMoreThanCreditLimit() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+
+        account.add(5_000);
+
+        Assertions.assertEquals(5_000, account.getBalance());
+        // * Может иметь баланс вплоть до отрицательного, но до указанного кредитного лимита.
+        //метод add класса CreditAccount должен возвращать false
+    }
+
+
+
+
 }
