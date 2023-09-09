55045c56677439ca3d33b2c09c9f786e4a62d249
dryworts <omzg@mail.ru>
2023-09-09 10:44:40 +0600

    add-tests added

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -17,37 +17,97 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
-
     @Test
-    public void shouldAddIfInitialMoreThanZero() {
-        CreditAccount account = new CreditAccount(
-                100,
-                3_000,
-                15
-        );
+    //пополнить баланс на положительную сумму
+    public void AddPositiveToBalance() {
 
-        account.add(3_000);
-
-        Assertions.assertNotEquals(3100, account.getBalance());
-//Метод add неверно реализован, т.к. должно быть не balance = amount, а balance += amount
-    }
-
-
-    @Test
-    public void shouldAddButMoreThanCreditLimit() {
         CreditAccount account = new CreditAccount(
                 0,
                 5_000,
                 15
         );
+        boolean expected = true;
+        boolean actual = account.add(1_000);
 
-        account.add(5_000);
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    //пополнить баланс на отрицательную сумму
+    public void AddNegativeToBalance() {
 
-        Assertions.assertEquals(5_000, account.getBalance());
-        // * Может иметь баланс вплоть до отрицательного, но до указанного кредитного лимита.
-        //метод add класса CreditAccount должен возвращать false
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                15
+        );
+        boolean expected = false;
+        boolean actual = account.add(-1_000);
+
+        Assertions.assertEquals(expected, actual);
     }
 
+    public void AddZeroToBalance() {
+
+        CreditAccount account = new CreditAccount(
+                10,
+                5_000,
+                15
+        );
+        boolean expected = false;
+        boolean actual = account.add(0);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+//
+//    @Test
+//    //проверка отрицательный баланс
+//    public void shouldAddToNegativeBalance() {
+//        CreditAccount account = new CreditAccount(
+//                0,
+//                5_000,
+//                15
+//        );
+//
+//        account.add(-3_000);
+//
+//        Assertions.assertEquals(3_000, account.getBalance());
+//    }
+//
+//
+//
+//
+//    @Test
+//    public void shouldAddIfInitialMoreThanZero() {
+//        CreditAccount account = new CreditAccount(
+//                100,
+//                3_000,
+//                15
+//        );
+//
+//        account.add(3_000);
+//
+//        Assertions.assertNotEquals(3100, account.getBalance());
+////Метод add неверно реализован, т.к. должно быть не balance = amount, а balance += amount
+//    }
+//
+//
+//    @Test
+//    public void shouldAddButMoreThanCreditLimit() {
+//        CreditAccount account = new CreditAccount(
+//                0,
+//                5_000,
+//                15
+//        );
+//
+//        account.add(5_000);
+//
+//        Assertions.assertEquals(5_000, account.getBalance());
+//        // * Может иметь баланс вплоть до отрицательного, но до указанного кредитного лимита.
+//        //метод add класса CreditAccount должен возвращать false
+//    }
+//
 
 
 
